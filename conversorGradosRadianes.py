ca01c6ab73b3910046149429422a69ee016ea2d7
Víctor Rodríguez <victor_jacholive@hotmail.com>
2022-02-19 01:20:50 -0500

    importe el modulo math y use la funcion math.pi()

--- conversorGradosRadianes.py
+++ conversorGradosRadianes.py
@@ -1,7 +1,11 @@
 # Relacion Grados sexagesimal y Radian
 
+import math
 
+PI = math.pi()
 GRADOS = 45
+RADIANES = 67
 
-def grados_Radianes():
-    pass
+def grados_Radianes(grados, radianes):
+    GRADOS = (radianes * 360) // (2 * PI)
+    return GRADOS 
