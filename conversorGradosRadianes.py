e0fde9edea703358baaaef4a4e8f3d07538453fa
Víctor Rodríguez <victor_jacholive@hotmail.com>
2022-02-19 01:32:45 -0500

    borre el modulo math.pi() porque me daba un error de tipo; no pude oprar con pi al ser float

--- conversorGradosRadianes.py
+++ conversorGradosRadianes.py
@@ -2,10 +2,12 @@
 
 import math
 
-PI = math.pi()
+PI = 3.1415
 GRADOS = 45
 RADIANES = 67
 
 def grados_Radianes(grados, radianes):
     GRADOS = (radianes * 360) // (2 * PI)
     return GRADOS 
+
+print(grados_Radianes(0, RADIANES))
