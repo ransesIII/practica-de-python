3e44a5ecec8f28826c03f2dba91e1369a68c3037
Víctor Rodríguez <victor_jacholive@hotmail.com>
2022-02-25 19:18:20 -0500

    primer cambio donfucionalidad

--- conversorGradosRadianes.py
+++ conversorGradosRadianes.py
@@ -3,16 +3,34 @@ def main():
 
     PI = 3.1415
 
-    def grados_Radianes(grados, radianes):
+    def DeRadianes_Grados(radianes):
         GRADOS = (radianes * 360) / (2 * PI)
         return GRADOS
+    
+    def DeGrados_Radianes(grados):
+        RADIANES = ((2 * PI) * grados) / 360
+        return RADIANES
 
     
-    GRADOS = 45
-    RADIANES = 2
+    INTRO = 'Convertidor de Grados/Radianes'
+    print(INTRO)
 
-    resultado = round(grados_Radianes(GRADOS, RADIANES), 2)
-    print(f'Tus radianes a grados son {resultado} grados')
+    OPCIONES = input('Elige una opcion de convercion; g para Radianes a Grados, o, r para Grados a Radianes: ')
+    print(OPCIONES)
+
+
+    if OPCIONES == 'g':
+        radianes = int(input('Escribe los Radianes: '))
+        resultadoGrados = round(DeRadianes_Grados(radianes), 2)
+        print(f'{resultadoGrados} Grados')
+    elif OPCIONES == 'r':
+        grados = int(input('Escribe los Grados: '))
+        resultadoRadianes = round(DeGrados_Radianes(grados), 2)
+        print(f'{resultadoRadianes} Radianes')
+    
+    
+    # resultado = round(grados_Radianes(), 2)
+    # print(f'Tus radianes a grados son {resultado} grados')
 
 
 if __name__ == '__main__':
